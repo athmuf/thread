d9aebfaa3e371026a975900d706a56880bafb5e7
athmuf <muflproject@gmail.com>
2026-03-04 06:36:54 +0700

    add login form component test

--- components/cuitin/auth/login-form.test.tsx
+++ components/cuitin/auth/login-form.test.tsx
@@ -7,16 +7,18 @@
  *   - should call login dispatch when login button is clicked
  */
 
-import { describe, expect, it, vi, beforeEach } from 'vitest';
+import { describe, expect, it, vi, beforeEach, afterEach } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import LoginForm from './login-form';
 import authReducer from '@/src/features/auth/auth-slice';
-import authDialogReducer from '@/src/features/auth-dialog/auth-dialog-slice';
+import authDialogReducer, {
+  openDialog,
+} from '@/src/features/auth-dialog/auth-dialog-slice';
+import { Dialog } from '@/components/ui/dialog';
 
-// Mock sonner
 vi.mock('sonner');
 
 const makeStore = () =>
@@ -27,11 +29,14 @@ const makeStore = () =>
     },
   });
 
-// Helper render with provider
 const renderWithProvider = (store = makeStore()) => {
+  store.dispatch(openDialog('login'));
+
   return render(
     <Provider store={store}>
-      <LoginForm />
+      <Dialog open={true}>
+        <LoginForm />
+      </Dialog>
     </Provider>,
   );
 };
@@ -41,13 +46,18 @@ describe('LoginForm component', () => {
     vi.clearAllMocks();
   });
 
+  afterEach(() => {
+    cleanup();
+  });
+
   it('should handle email typing correctly', async () => {
     // Arrange
     renderWithProvider();
     const emailInput = screen.getByPlaceholderText('johndoe@example.com');
 
     // Action
-    await userEvent.type(emailInput, 'john@example.com');
+    const user = userEvent.setup({ delay: null });
+    await await user.type(emailInput, 'john@example.com');
 
     // Assert
     expect(emailInput).toHaveValue('john@example.com');
@@ -59,7 +69,8 @@ describe('LoginForm component', () => {
     const passwordInput = screen.getByLabelText('Password');
 
     // Action
-    await userEvent.type(passwordInput, 'password123');
+    const user = userEvent.setup({ delay: null });
+    await user.type(passwordInput, 'password123');
 
     // Assert
     expect(passwordInput).toHaveValue('password123');
@@ -71,14 +82,12 @@ describe('LoginForm component', () => {
     vi.spyOn(store, 'dispatch');
     renderWithProvider(store);
 
-    await userEvent.type(
-      screen.getByPlaceholderText('johndoe@example.com'),
-      'john@example.com',
-    );
-    await userEvent.type(screen.getByLabelText('Password'), 'password123');
+    const user = userEvent.setup({ delay: null });
+    await user.type(screen.getByLabelText(/email/i), 'john@example.com');
+    await user.type(screen.getByLabelText('Password'), 'password123');
 
     // Action
-    await userEvent.click(screen.getByRole('button', { name: /loqgin/i }));
+    await user.click(screen.getByRole('button', { name: /login/i }));
 
     // Assert
     expect(store.dispatch).toHaveBeenCalled();
